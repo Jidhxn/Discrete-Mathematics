c12ae9b52bc60673a37cfd1ffd3186ad0a140c82
Jidhun <ashchiquu@gmail.com>
2022-07-26 10:46:07 -0700

    Update Q3-(TowerOfHanoi).cpp

--- Q3-(TowerOfHanoi).cpp
+++ Q3-(TowerOfHanoi).cpp
@@ -1,6 +1,38 @@
 #include<iostream>
 using namespace std;
 
+//tower of HANOI function implementation
+void TOH(int n,char Sour, char Aux,char Des)
+{ 
+	if(n==1)
+	{
+		cout<<"Move Disk "<<n<<" from "<<Sour<<" to "<<Des<<endl;
+		return;
+	}
+	
+	TOH(n-1,Sour,Des,Aux);
+	cout<<"Move Disk "<<n<<" from "<<Sour<<" to "<<Des<<endl;
+	TOH(n-1,Aux,Sour,Des);
+}
+
+//main program
+int main()
+{ 
+	int n;
+	
+	cout<<"Enter no. of disks:";	
+	cin>>n;
+	//calling the TOH 
+	TOH(n,'A','B','C');
+	
+	return 0;
+}
+
+#For returning number of moves
+
+/*#include<iostream>
+using namespace std;
+
 int TOH(int n)
 {
     if(n==1)
@@ -16,4 +48,4 @@ int main()
     cin>>n;
     m = TOH(n);
     cout<<"\nNo. of moves taken are: "<<m;
-}
+}*/
